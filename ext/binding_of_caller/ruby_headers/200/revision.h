526269b2bb19761ea89c0877dc532325ed8a8b4d
John Mair <jrmair@gmail.com>
2013-02-12 23:55:26 +0100

    remove ruby 2.0 headers
    no longer need these as we're using debug_inspector gem instead.

--- ext/binding_of_caller/ruby_headers/200/revision.h
+++
@@ -1,1 +0,0 @@
-#define RUBY_REVISION 37260
--- ext/binding_of_caller/ruby_headers/200/verconf.h
+++
@@ -1,5 +0,0 @@
-#define RUBY_LIB_VERSION_STYLE 3	/* full */
-#define RUBY_LIB_PREFIX RUBY_EXEC_PREFIX"/lib/ruby"
-#define RUBY_SITE_LIB RUBY_LIB_PREFIX"/site_ruby"
-#define RUBY_VENDOR_LIB RUBY_LIB_PREFIX"/vendor_ruby"
-#define RUBY_PLATFORM "x86_64-linux"
