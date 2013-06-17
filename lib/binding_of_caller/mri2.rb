8689d34908aca71f373aedcc7d3c7c81d02d7ed4
Amadeus Folego <amadeusfolego@gmail.com>
2013-06-17 11:08:10 -0300

    Avoid loops and call of expensive methods

--- lib/binding_of_caller/mri2.rb
+++ lib/binding_of_caller/mri2.rb
@@ -5,61 +5,58 @@ module BindingOfCaller
     # Retrieve the binding of the nth caller of the current frame.
     # @return [Binding]
     def of_caller(n)
-      c = callers.drop(1)
-      if n > (c.size - 1)
-        raise "No such frame, gone beyond end of stack!"
-      else
-        c[n]
-      end
+      RubyVM::DebugInspector.open { |i| setup_binding_from_location(i, n+2) }
+    rescue
+      raise "No such frame, gone beyond end of stack!"
     end
 
     # Return bindings for all caller frames.
     # @return [Array<Binding>]
     def callers
-      ary = []
-    
       RubyVM::DebugInspector.open do |i|
-        n = 0
-        loop do
-          begin
-            b = i.frame_binding(n) 
-          rescue ArgumentError
-            break
-          end
-
-          if b
-            b.instance_variable_set(:@iseq, i.frame_iseq(n))
-            ary << b
-          end
-          
-          n += 1
+        (2..(i.backtrace_locations.count-2)).map do |n|
+          setup_binding_from_location(i, n)
         end
       end
-      
-      ary.drop(1)
     end
 
     # Number of parent frames available at the point of call.
     # @return [Fixnum]
     def frame_count
-      callers.size - 1
+      RubyVM::DebugInspector.open(&:backtrace_locations).count - 3
     end
 
     # The type of the frame.
     # @return [Symbol]
     def frame_type
-      return nil if !@iseq
-      
-      # apparently the 9th element of the iseq array holds the frame type
-      # ...not sure how reliable this is.
-      @frame_type ||= @iseq.to_a[9]
+      if not @iseq.nil?
+        # apparently the 9th element of the iseq array holds the frame type
+        # ...not sure how reliable this is.
+        @frame_type ||= @iseq.to_a[9]
+      end
     end
 
     # The description of the frame.
     # @return [String]
     def frame_description
-      return nil if !@iseq
-      @frame_description ||= @iseq.label
+      if not @iseq.nil?
+        @frame_description ||= @iseq.label
+      end
+    end
+
+    protected
+
+    def setup_binding_from_location(inspector, frame_number)
+      binding = inspector.frame_binding(frame_number)
+
+      if binding.nil?
+        binding = setup_binding_from_location(inspector, frame_number + 1)
+      end
+
+      binding.instance_variable_set(:@iseq,
+                                    inspector.frame_iseq(frame_number))
+
+      binding
     end
   end
 end
