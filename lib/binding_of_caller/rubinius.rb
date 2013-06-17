780e9e819f517fa220c12920e168f6fcd775d081
Amadeus Folego <amadeusfolego@gmail.com>
2013-06-17 10:30:25 -0300

    Reuse code that sets up binding from location

--- lib/binding_of_caller/rubinius.rb
+++ lib/binding_of_caller/rubinius.rb
@@ -4,22 +4,11 @@ module BindingOfCaller
     # Retrieve the binding of the nth caller of the current frame.
     # @return [Binding]
     def of_caller(n)
-      bt = Rubinius::VM.backtrace(1 + n, true).first
+      location = Rubinius::VM.backtrace(1 + n, true).first
 
-      raise RuntimeError, "Invalid frame, gone beyond end of stack!" if bt.nil?
+      raise RuntimeError, "Invalid frame, gone beyond end of stack!" if location.nil?
 
-      b = Binding.setup(
-                        bt.variables,
-                        bt.variables.method,
-                        bt.constant_scope,
-                        bt.variables.self,
-                        bt
-                        )
-
-      b.instance_variable_set :@frame_description,
-                              bt.describe.gsub("{ } in", "block in")
-
-      b
+      setup_binding_from_location(location)
     end
 
     # The description of the frame.
@@ -31,19 +20,8 @@ module BindingOfCaller
     # Return bindings for all caller frames.
     # @return [Array<Binding>]
     def callers
-      ary = []
-      n = 0
-      loop do
-        begin
-          ary << Binding.of_caller(n)
-        rescue
-          break
-        end
-        n += 1
-      end
-      ary.drop_while do |v|
-        !(v.frame_type == :method && v.eval("__method__") == :callers)
-      end.drop(1)
+      Rubinius::VM.backtrace(1, true).map &(method(:setup_binding_from_location).
+                                            to_proc)
     end
 
     # Number of parent frames available at the point of call.
@@ -64,6 +42,21 @@ module BindingOfCaller
       else
         :method
       end
+    end
+
+    protected
+
+    def setup_binding_from_location(location)
+      binding = Binding.setup location.variables,
+                              location.variables.method,
+                              location.constant_scope,
+                              location.variables.self,
+                              location
+
+      binding.instance_variable_set :@frame_description,
+                                   location.describe.gsub("{ } in", "block in")
+
+      binding
     end
   end
 end
