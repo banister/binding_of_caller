b947e35ac3b5f74e4aee2fd884efb63ef3bd201d
Stefan Kaes <stefan.kaes@xing.com>
2014-01-07 17:43:58 +0100

    use $INCLFLAGS instead of $CFLAGS. fixes #42.

--- ext/binding_of_caller/extconf.rb
+++ ext/binding_of_caller/extconf.rb
@@ -17,9 +17,9 @@ if mri_1_9?
 
   case RUBY_VERSION
   when /1.9.2/
-    $CFLAGS += " -I./ruby_headers/192/ -DRUBY_192"
+    $INCFLAGS += " -I./ruby_headers/192/ -DRUBY_192"
   when /1.9.3/
-    $CFLAGS += " -I./ruby_headers/193/ -DRUBY_193"
+    $INCFLAGS += " -I./ruby_headers/193/ -DRUBY_193"
   end
 
   create_makefile('binding_of_caller')
