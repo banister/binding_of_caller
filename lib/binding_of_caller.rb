7c094856f6b8f43c600f37bfdf62cf04cfe2112c
Akira Matsuda <ronnie@dio.jp>
2017-06-14 21:09:27 +0900

    Don't pollute toplevel with mri_2? method

--- lib/binding_of_caller.rb
+++ lib/binding_of_caller.rb
@@ -1,11 +1,9 @@
 dlext = RbConfig::CONFIG['DLEXT']
 
-def mri_2?
-  defined?(RUBY_ENGINE) && RUBY_ENGINE == "ruby" &&
+mri_2 = defined?(RUBY_ENGINE) && RUBY_ENGINE == "ruby" &&
     RUBY_VERSION =~ /^2/
-end
 
-if mri_2?
+if mri_2
   require 'binding_of_caller/mri2'
 elsif defined?(RUBY_ENGINE) && RUBY_ENGINE == "ruby"
   require "binding_of_caller.#{dlext}"
