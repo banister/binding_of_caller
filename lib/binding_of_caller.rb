1488dbe347b932e6a8bd94c90fe0426aa6a0f2c6
Thorben Starsy <info@thorbenschroeder.de>
2020-12-25 21:03:40 +0100

    Makes loading work with Ruby 3

--- lib/binding_of_caller.rb
+++ lib/binding_of_caller.rb
@@ -1,9 +1,12 @@
 dlext = RbConfig::CONFIG['DLEXT']
 
-mri_2 = defined?(RUBY_ENGINE) && RUBY_ENGINE == "ruby" &&
-    RUBY_VERSION =~ /^2/
+mri = defined?(RUBY_ENGINE) && RUBY_ENGINE == "ruby"
 
-if mri_2
+mri_2 = mri && RUBY_VERSION =~ /^2/
+
+mri_3 = mri && RUBY_VERSION =~ /^3/
+
+if mri_2 || mri_3
   require 'binding_of_caller/mri2'
 elsif defined?(RUBY_ENGINE) && RUBY_ENGINE == "ruby"
   require "binding_of_caller.#{dlext}"
